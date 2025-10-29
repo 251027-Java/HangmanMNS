ae0d0b4596e41b14d8da7130a2e9f600db877e81
Sidney Gil <sidneygil@Sidneys-MacBook-Air.local>
2025-10-29 14:13:28 -0500

    psuedo code

--- src/main/java/HangmanGame.java
+++ src/main/java/HangmanGame.java
@@ -1,2 +1,13 @@
+import java.util.Scanner;
+
 public class HangmanGame {
+    public static void main() {
+        // array of words, picked at random (or one word)
+        // give the person the length of the word
+        // prompt the user for a guess
+        // if right add it in the word with dashes
+        // if not, add to the bucket list and tally incorrect guesses and incorrect guesses remaining
+        // if person runout of guesses game ends
+        // if guesses the word correctly the person wins and game ends
+    }
 }
