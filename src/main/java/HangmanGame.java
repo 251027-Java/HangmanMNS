12b5e19395fb8f7ad44fc9ea6e05cb6958b75a27
manas <manasvini569@revature.net>
2025-10-29 14:59:46 -0500

    Rough outline of logic with loop and game end conditions

--- src/main/java/HangmanGame.java
+++ src/main/java/HangmanGame.java
@@ -12,5 +12,35 @@ public class HangmanGame {
         // Manu
         // if person runout of guesses game ends
         // if guesses the word correctly the person wins and game ends
+
+        // Made a rough outline so everyone can add logic in their part
+        // Feel free to replace whatever part of this code
+        boolean playing = true;
+        int mistakes = 0;
+        while(playing)
+        {
+            // game played in here
+            String word = ""; // the randomly picked word
+            char guess = 'e';  // the guess
+            char[] currentProgress = new char[word.length()];
+
+            // each guess either adds to mistakes
+            if(word.indexOf(guess) == -1) {
+                mistakes++;
+            }
+            // or is added to currentProgress
+            else {
+                // probably use indexOf and add letter to currentProgress at that index
+                currentProgress[0] = 'y';
+            }
+
+            if(mistakes == 7) {
+                playing = false;
+                IO.println("Sorry, no more guesses left. Game over.");
+            } else if(currentProgress.toString().equals(word)) {
+                playing = false;
+                IO.println("Congrats! You won the game.");
+            }
+        }
     }
 }
