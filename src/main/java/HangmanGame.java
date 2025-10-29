5aac8637a1f1d8b91c3939e2b0b6e982961b5b84
Sidney Gil <sidneygil@Sidneys-MacBook-Air.local>
2025-10-29 15:19:31 -0500

    random word generated

--- src/main/java/HangmanGame.java
+++ src/main/java/HangmanGame.java
@@ -1,13 +1,24 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class HangmanGame {
     public static void main() {
+        Scanner scanner = new Scanner(System.in);
         // array of words, picked at random (or one word)
+        Random random = new Random();
+        String[] myArray = {"spiderman", "superman", "ironman", "batman"};
+        int randomIndex = random.nextInt(myArray.length);
+        System.out.println(myArray[randomIndex]);
+        String blankArray = myArray[randomIndex];
+
         // give the person the length of the word
+
+
         // prompt the user for a guess
         // if right add it in the word with dashes
         // if not, add to the bucket list and tally incorrect guesses and incorrect guesses remaining
         // if person runout of guesses game ends
         // if guesses the word correctly the person wins and game ends
+
     }
 }
