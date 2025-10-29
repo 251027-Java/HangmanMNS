f8cb983adb93e827d6e151e30d414d1de6b8f48b
Sidney Gil <sidneygil@Sidneys-MacBook-Air.local>
2025-10-29 15:33:36 -0500

    print blank word to user

--- src/main/java/HangmanGame.java
+++ src/main/java/HangmanGame.java
@@ -1,17 +1,31 @@
 import java.util.Scanner;
 import java.util.Random;
+import java.util.Arrays;
 
 public class HangmanGame {
     public static void main() {
         Scanner scanner = new Scanner(System.in);
         // array of words, picked at random (or one word)
         Random random = new Random();
-        String[] myArray = {"spiderman", "superman", "ironman", "batman"};
+        String[] myArray = {"spiderman", "superman", "ironman", "batman", "thor", "flash"};
         int randomIndex = random.nextInt(myArray.length);
-        System.out.println(myArray[randomIndex]);
-        String blankArray = myArray[randomIndex];
 
         // give the person the length of the word
+        //create integer to establish array size
+        int a = 0;
+        for (int i = 0; i < myArray[randomIndex].length(); i++ ){
+            a = a + 1;
+        }
+
+        // create an array size using the previous int
+        String[] blankWord = new String[a];
+
+        //Fill each element in array with "_"
+        String fillChar = "_";
+        Arrays.fill(blankWord, fillChar);
+
+        // Print out blankWord
+        System.out.print(Arrays.toString(blankWord));
 
 
         // prompt the user for a guess
