8689fdab6141b7833632bd97abf70d4b0d1fcf6e
manas <manasvini569@revature.net>
2025-10-29 14:29:05 -0500

    First commit on MBranch

--- src/main/java/HangmanGame.java
+++ src/main/java/HangmanGame.java
@@ -5,9 +5,14 @@ public class HangmanGame {
         // array of words, picked at random (or one word)
         // give the person the length of the word
         // prompt the user for a guess
+
         // if right add it in the word with dashes
         // if not, add to the bucket list and tally incorrect guesses and incorrect guesses remaining
+
+        // Manu
         // if person runout of guesses game ends
         // if guesses the word correctly the person wins and game ends
+
+
     }
 }
