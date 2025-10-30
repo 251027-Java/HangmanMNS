2c202e71dc07cbf557729b263ad3da67d2cad626
Natalia Orlof-Carson <norlofcarson@sandiego.edu>
2025-10-30 10:59:58 -0500

    Added the option to play again

--- src/main/java/HangmanGame.java
+++ src/main/java/HangmanGame.java
@@ -14,82 +14,83 @@ public class HangmanGame {
         String[] words = content.split(",\\s*");
 
         Random rand = new Random();
-        String word = words[rand.nextInt(words.length)];
 
+        boolean playAgain = false;
+        do {
+            String word = words[rand.nextInt(words.length)];
 
-        // create an array size using the previous int
-        char[] blankWord = new char[word.length()];      //The Array of the guess so far
+            // create an array size using the previous int
+            char[] blankWord = new char[word.length()];      //The Array of the guess so far
 
-        //Fill each element in array with "_"
-        char fillChar = '_';
-        Arrays.fill(blankWord, fillChar);
+            //Fill each element in array with "_"
+            char fillChar = '_';
+            Arrays.fill(blankWord, fillChar);
 
-        // Print out blankWord
-        //System.out.print(Arrays.toString(blankWord));
-        for (char c : blankWord) {
-            System.out.print(c + " ");
-        }
-        System.out.println();
-
-
-        char guessLetter;       //The user's current guess letter
-        guessLetter = '_';
-
-        List<Character> incorrectGuesses = new ArrayList<>();       //the list of the incorrect characters
-
-        int guessesLeft = 7;        //The amount of incorrect guesses the user has left
-        boolean playing = true;     //Denotes if we are still playing the game
-
-        //Main loop to continue playing until the word was guessed or there are no guesses left
-        while(guessesLeft > 0 && playing){
-            boolean correct = false;
-
-            //Verifies that the entry of the character was correct
-            while (!correct) {
-                String guess = IO.readln("\nPlease enter a guess: ");
-                char enteredChar = guess.charAt(0);
-                Pattern p = Pattern.compile("[^a-z]");
-                Matcher m = p.matcher(guess);
-                if (Arrays.equals(guess.toCharArray(), word.toCharArray())) {
-                    playing = false;
-                    IO.println("Congrats! You won the game.");
-                    return;
-                } else if (m.find()) {
-                    IO.println("Your guess was invalid, please guess again");
-                } else if (guess.length() != 1) {
-                    IO.println("Your guess was invalid, please guess again");
-                } else if ((m.find())){
-                    IO.println("Your guess was invalid, please guess again");
-                } else if(new String(blankWord).indexOf(enteredChar) != -1 || incorrectGuesses.contains(enteredChar)) {
-                    IO.println("You already guessed this letter, please guess again");
-                } else{
-                    correct = true;
-                    guessLetter = guess.charAt(0);
-                }
-
+            // Print out blankWord
+            //System.out.print(Arrays.toString(blankWord));
+            for (char c : blankWord) {
+                System.out.print(c + " ");
             }
+            System.out.println();
 
-            boolean guessFound = false;
-            // If guess is right, adds the letter into display
-            if(word.indexOf(guessLetter) != -1)
-            {
-                guessFound = true;
-                //Runs through the word to see if there are any matching iteracies
-                for(int i = 0; i < word.length(); i++)
-                {
-                    if(word.charAt(i) == guessLetter) {
-                        blankWord[i] = word.charAt(i);
+
+            char guessLetter;       //The user's current guess letter
+            guessLetter = '_';
+
+            List<Character> incorrectGuesses = new ArrayList<>();       //the list of the incorrect characters
+
+            int guessesLeft = 7;        //The amount of incorrect guesses the user has left
+            boolean playing = true;     //Denotes if we are still playing the game
+
+            //Main loop to continue playing until the word was guessed or there are no guesses left
+            outerLoop:
+            while (guessesLeft > 0 && playing) {
+                boolean correct = false;
+
+                //Verifies that the entry of the character was correct
+                while (!correct) {
+                    String guess = IO.readln("\nPlease enter a guess: ");
+                    char enteredChar = guess.charAt(0);
+                    Pattern p = Pattern.compile("[^a-z]");
+                    Matcher m = p.matcher(guess);
+                    if (Arrays.equals(guess.toCharArray(), word.toCharArray())) {
+                        playing = false;
+                        IO.println("Congrats! You won the game.");
+                        break outerLoop;
+                    } else if (m.find()) {
+                        IO.println("Your guess was invalid, please guess again");
+                    } else if (guess.length() != 1) {
+                        IO.println("Your guess was invalid, please guess again");
+                    } else if ((m.find())) {
+                        IO.println("Your guess was invalid, please guess again");
+                    } else if (new String(blankWord).indexOf(enteredChar) != -1 || incorrectGuesses.contains(enteredChar)) {
+                        IO.println("You already guessed this letter, please guess again");
+                    } else {
+                        correct = true;
+                        guessLetter = guess.charAt(0);
                     }
+
                 }
 
-                // display
-                IO.println("Your guess was correct.");
-                IO.println(makeDrawing(guessesLeft));
-                for (char c : blankWord) {
-                    System.out.print(c + " ");
-                }
-                System.out.println();
-                IO.println("Incorrect guesses: " + incorrectGuesses);
+                boolean guessFound = false;
+                // If guess is right, adds the letter into display
+                if (word.indexOf(guessLetter) != -1) {
+                    guessFound = true;
+                    //Runs through the word to see if there are any matching iteracies
+                    for (int i = 0; i < word.length(); i++) {
+                        if (word.charAt(i) == guessLetter) {
+                            blankWord[i] = word.charAt(i);
+                        }
+                    }
+
+                    // display
+                    IO.println("Your guess was correct.");
+                    IO.println(makeDrawing(guessesLeft));
+                    for (char c : blankWord) {
+                        System.out.print(c + " ");
+                    }
+                    System.out.println();
+                    IO.println("Incorrect guesses: " + incorrectGuesses);
 
                     // if the entire word has been guessed, the game has been won
 
@@ -98,35 +99,54 @@ public class HangmanGame {
                         IO.println("Congrats! You won the game.");
                     }
 
+                }
+
+                //if the guess was wrong, it lets the users know how many have left and the letters they have chosen
+                if (!guessFound) {
+                    IO.println("Your guess was incorrect.");
+
+                    // Guesses left is decremented
+                    guessesLeft -= 1;
+
+                    IO.println(makeDrawing(guessesLeft));
+                    incorrectGuesses.add(guessLetter);
+                    IO.println("You have " + guessesLeft + " more wrong guesses.");
+                    IO.println("Incorrect guesses: " + incorrectGuesses.toString());
+
+                    for (char c : blankWord) {
+                        System.out.print(c + " ");
+                    }
+                    System.out.println();
+
+                    // if there are no more guesses left, game ends
+                    if (guessesLeft == 0) {
+                        IO.println("No more guesses left, game over.");
+                        IO.println("The word was: " + word);
+                    }
+                }
+                IO.println();
             }
 
-            //if the guess was wrong, it lets the users know how many have left and the letters they have chosen
-            if (!guessFound) {
-                IO.println("Your guess was incorrect.");
-
-                // Guesses left is decremented
-                guessesLeft -= 1;
-
-                IO.println(makeDrawing(guessesLeft));
-                incorrectGuesses.add(guessLetter);
-                IO.println("You have " + guessesLeft + " more wrong guesses.");
-                IO.println("Incorrect guesses: " + incorrectGuesses.toString());
-
-                for (char c : blankWord) {
-                    System.out.print(c + " ");
-                }
-                System.out.println();
-
-                // if there are no more guesses left, game ends
-                if (guessesLeft == 0){
-                    IO.println("No more guesses left, game over.");
-                    IO.println("The word was: " + word);
-                }
+            String playResponse = IO.readln("Would you like to play again? (Y/N): ");
+            if (playResponse == null) {           // user hit EOF/cancel
+                System.out.println("Goodbye!");
+                break;
             }
-            IO.println();
+
+            String ans = playResponse.trim().toUpperCase();
+
+            if (ans.equals("Y")) {
+                playAgain = true;                 // loop continues
+            } else if (ans.equals("N")) {
+                playAgain = false;                // loop ends
+            } else {
+                System.out.println("Invalid response. Ending game.");
+                playAgain = false;                // end on invalid
+            }
+        }while(playAgain);
+
+
         }
-
-    }
 
     // Returns the hangman drawing for each turn
     // The drawing updates based on the number of incorrect guesses left
